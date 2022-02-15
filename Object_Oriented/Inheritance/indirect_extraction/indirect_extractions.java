6db78363993a6af4ac2160e48e991bd7bde3b43e
Warmcloud1 <52983230+Warmcloud1@users.noreply.github.com>
2022-02-15 10:45:00 +0800

    Create indirect_extractions.java

---
+++ Object_Oriented/Inheritance/indirect_extraction/indirect_extractions.java
@@ -0,0 +1,19 @@
+package first;
+
+import Animal_test.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Cat cat=new Cat();
+		cat.getNum();
+  }
+}
+//*****************************************
+package Animal_test;
+public class Cat extends Animal{
+	private int num=1; //成員用private
+  //但可用public間接提取，大部分情況希望能這樣做
+	public int getNum() {
+		return num;
+	}
+}
