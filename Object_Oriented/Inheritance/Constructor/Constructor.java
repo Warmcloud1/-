10f9abf76728fc32c577b98068cb73832a7e160f
Warmcloud1 <52983230+Warmcloud1@users.noreply.github.com>
2022-02-18 04:40:06 +0800

    Update Constructor.java

--- Object_Oriented/Inheritance/Constructor/Constructor.java
+++ Object_Oriented/Inheritance/Constructor/Constructor.java
@@ -13,7 +13,7 @@ public class Main {
 package Animal_test;
 
 public class Animal {
-	public Animal(int a) {
+	public Animal(int a) { //輸入a
 		System.out.println("動物的建構子");
 	}
 	public void eat() {
@@ -25,9 +25,9 @@ public class Animal {
 package Animal_test;
 
 public class Cat extends Animal{
-	public Cat() {
+	public Cat() {//輸入空
 		super(1);回傳父類a值
-   		//super指的是當前操作父類的實體
+   		//super指的是當前操作父類的實體，也可以super方法
     		//this指的是當前操作類的實體
 		System.out.println("貓咪的建構子");
 	}
